9696605702c41e343cfa7eab6a41ae12d6efe62e
michaelainnovphil <mdbarreda@innovphil.com>
2025-04-02 17:28:27 +0800

    OH HELL NAH

--- pages/index.js
+++ pages/index.js
@@ -90,21 +90,20 @@ export default function Home() {
       <div style={{ marginTop: '20px' }}>
         <h2 style={{ color: 'white' }}>Messages</h2>
         <ul style={{ listStyle: 'none', paddingLeft: '0' }}>
-        {messages.map((msg, index) => (
-  <li
-    key={index}
-    style={{
-      marginBottom: '10px',
-      padding: '10px',
-      border: '1px solid #ddd',
-      borderRadius: '5px',
-      backgroundColor: '#fff',
-    }}
-  >
-    {msg.content} {/* Access content of the message */}
-  </li>
-))}
-
+          {messages.map((msg, index) => (
+            <li
+              key={index}
+              style={{
+                marginBottom: '10px',
+                padding: '10px',
+                border: '1px solid #ddd',
+                borderRadius: '5px',
+                backgroundColor: '#fff',
+              }}
+            >
+              {msg}
+            </li>
+          ))}
         </ul>
       </div>
     </div>
